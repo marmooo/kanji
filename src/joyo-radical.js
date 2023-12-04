128f8be32ae9fc23af95079a2b03384ad0a18339
marmooo <marmooo@users.noreply.github.com>
2023-12-04 12:18:02 +0900

    fix copy bug

--- src/joyo-radical.js
+++ src/joyo-radical.js
@@ -3,7 +3,7 @@ import { Joyo } from "./jinmei.js";
 import { Unicode1Radical } from "./unicode1-radical.js";
 
 const joyo = new Kanji([Joyo]);
-export const JoyoRadical = Unicode1Radical;
+export const JoyoRadical = structuredClone(Unicode1Radical);
 JoyoRadical.filter((list, grade) => {
   JoyoRadical[grade] = list.filter((kanji) => joyo.getGrade(kanji) >= 0);
 });
