a9cf861e9d702333f484b188a2624210bf60f721
marmooo <marmooo@users.noreply.github.com>
2023-12-04 12:46:59 +0900

    support Node.js 16

--- src/joyo-radical-strokes.js
+++ src/joyo-radical-strokes.js
@@ -3,7 +3,9 @@ import { Joyo } from "./jinmei.js";
 import { Unicode1RadicalStrokes } from "./unicode1-radical-strokes.js";
 
 const joyo = new Kanji([Joyo]);
-export const JoyoRadicalStrokes = structuredClone(Unicode1RadicalStrokes);
+export const JoyoRadicalStrokes = JSON.parse(
+  JSON.stringify(Unicode1RadicalStrokes),
+);
 JoyoRadicalStrokes.filter((list, grade) => {
   JoyoRadicalStrokes[grade] = list.filter((kanji) => joyo.getGrade(kanji) >= 0);
 });
