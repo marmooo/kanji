225eb0732ba44154fa8d2b28394e81d8df269965
marmooo <marmooo@users.noreply.github.com>
2024-01-01 10:20:48 +0900

    0.0.8

--- build_npm.js
+++ build_npm.js
@@ -10,7 +10,7 @@ await build({
   },
   package: {
     name: "@marmooo/kanji",
-    version: "0.0.7",
+    version: "0.0.8",
     description: "Get various grade information about kanji",
     license: "MIT",
     main: "mod.js",
